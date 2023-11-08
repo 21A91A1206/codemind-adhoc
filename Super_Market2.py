bc7a0ca3d0ef0e2fe360a6db104c598ff26423c6
21A91A1206 <98875470+21A91A1206@users.noreply.github.com>
2023-11-08 14:56:07 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     if i not in l1 and l.count(i)==1:         l1.append(i) if(len(l1)==0):     print(-1) else:     for i in l1:         print(i,end=" ")

---
+++ Super_Market2.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in l:
+    if i not in l1 and l.count(i)==1:
+        l1.append(i)
+if(len(l1)==0):
+    print(-1)
+else:
+    for i in l1:
+        print(i,end=" ")
